ffd36d438cc3632d34a2a87c651c90d87b5d50ed
MafoudohEmmanuelle <mafoudohemmanuelle@gmail.com>
2025-05-23 15:11:17 +0100

    Initial commit for Railway deployment

--- portail/portail/settings.py
+++ portail/portail/settings.py
@@ -11,10 +11,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+#SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-_awyzvk=dvs@e36gxf8+gbbvo9)@dxy$ownffgg=$^1h^c*62j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+#DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG=False
+
+DATABASE_URL='postgresql://postgres:maman20201d@localhost/portail'
 
 ALLOWED_HOSTS = ['*']
 
@@ -78,6 +82,12 @@ DATABASES = {
     )
 }
 #  Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mafoudohemmanuelle@gmail.com'
+EMAIL_HOST_PASSWORD = 'm2d0e0e5'  
 
 """
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -85,14 +95,13 @@ EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
-"""
-
 
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT", cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
