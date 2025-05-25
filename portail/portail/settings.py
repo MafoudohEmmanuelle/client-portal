7de236b7a09fd84165365d11a4fb39a4fd825ea4
MafoudohEmmanuelle <mafoudohemmanuelle@gmail.com>
2025-05-25 20:27:27 +0100

    Fix static files for Railway deployment

--- portail/portail/settings.py
+++ portail/portail/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = ['client-portal-production-7b2f.up.railway.app','localhost,127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
