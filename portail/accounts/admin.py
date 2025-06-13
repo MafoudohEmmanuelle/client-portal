6f6de9e8105375a12ddddf688fc36501aa7d58e8
MafoudohEmmanuelle <m2d0e0e5>
2025-06-13 05:41:35 +0100

    Revised functionalities

--- portail/accounts/admin.py
+++ portail/accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import User,Client,Commercial,LeadRequest,BE,ChefCommercial
 from accounts.views import send_activation_email
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 admin.site.register(User)
 admin.site.register(Client)
