ab5b627751c99ae145bba6c6f315c4aa8f18ef88
MafoudohEmmanuelle <m2d0e0e5>
2025-06-13 05:37:43 +0100

    Revised functionalities

--- portail/accounts/admin.py
+++ portail/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import User,Client,Commercial,LeadRequest,BE,ChefCommercial
 from accounts.views import send_activation_email
+from django.contrib.auth import get_user_model
 
 admin.site.register(User)
 admin.site.register(Client)
@@ -9,15 +10,19 @@ admin.site.register(LeadRequest)
 admin.site.register(BE)
 admin.site.register(ChefCommercial)
 
-class UserAdmin(admin.ModelAdmin):
+User = get_user_model()
+# Unregister the existing User model
+admin.site.unregister(User)
+
+class UserAdmin(BaseUserAdmin):
     def save_model(self, request, obj, form, change):
         if not change:
-            obj.is_active = False  # User inactive until activation
+            obj.is_active = False
             obj.save()
-
-            # Call the activation email function
             send_activation_email(request, obj)
         else:
             super().save_model(request, obj, form, change)
 
+# Register it again with the custom admin
 admin.site.register(User, UserAdmin)
+
