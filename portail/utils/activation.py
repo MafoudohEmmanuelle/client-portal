805a89f1e8238fd556535f2b485fd2a5930db1f2
MafoudohEmmanuelle <m2d0e0e5>
2025-06-14 05:04:51 +0100

    fix : update form save methods for correct user creation

--- portail/utils/activation.py
+++ portail/utils/activation.py
@@ -1,25 +1,10 @@
-import hashlib
-import time
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+import six
 
-TOKEN_SALT = 'my_secret_salt'
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.is_active)
+        )
 
-def generate_activation_token(user):
-    """Generate a secure hash token based on user id and a secret salt."""
-    timestamp = int(time.time())  # Optional: Can use it to add expiration later
-    data = f'{user.pk}{TOKEN_SALT}{timestamp}'
-    token = hashlib.sha256(data.encode()).hexdigest()
-    return token, timestamp
-
-def verify_activation_token(user, token, timestamp, expiration=3600):
-    """Verify if the token is still valid and correctly generated."""
-    expected_data = f'{user.pk}{TOKEN_SALT}{timestamp}'
-    expected_token = hashlib.sha256(expected_data.encode()).hexdigest()
-
-    if expected_token != token:
-        return False
-
-    current_time = int(time.time())
-    if current_time - timestamp > expiration:
-        return False
-
-    return True
+account_activation_token = AccountActivationTokenGenerator()
